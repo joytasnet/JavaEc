3847e2b5059acdedd1ce952f3b606a347ab432ba
mjpurin <mjpurin@gmail.com>
2022-04-26 14:25:22 +0900

    day1終了

--- day1/src/day1/Sample.java
+++ day1/src/day1/Sample.java
@@ -1,10 +1,21 @@
 package day1;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("World");
 		System.out.println("Bye");
 		System.out.println("Byebye");
+
+		Map<String,Integer> map
+		=new HashMap<>();
+		map.put("東京", 1300);
+		map.put("神奈川" ,900);
+		map.put("埼玉" ,740);
+		System.out.println(map);
 
 	}
 
